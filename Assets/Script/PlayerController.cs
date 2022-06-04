73a6e6ce12364fb40342f63b0941d6e86f091ee1
zerovanisu <mazinari310@gmail.com>
2022-06-04 18:58:15 +0900

    プレイヤー実装

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rbody2D;
+
+    [Header("プレイヤーのジャンプ力")]
+    [SerializeField]
+    private float jumpForce = 350f;
+
+    private int jumpCount = 0;
+
+    void Start()
+    {
+        rbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && this.jumpCount < 1)
+        {
+            this.rbody2D.AddForce(transform.up * jumpForce);
+            jumpCount++;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            jumpCount = 0;
+            Debug.Log("着地");
+        }
+    }
+}
